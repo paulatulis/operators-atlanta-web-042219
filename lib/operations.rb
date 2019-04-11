24566729f162dadf22429e5ae317cb8b12edb276
Paula Tulis <tulis.paula@gmail.com>
2019-04-11 21:29:31 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,17 +1,13 @@
-
+def unsafe?(speed)
+if speed < 40 || speed > 60
+  true
+else
+  false
+end
+end
 
 
 
 def not_safe?(speed)
 	speed < 40 || speed > 60 ? true : false
-end
-	
-
-
-def unsafe?(speed)
-if speed > 40 && speed < 60
-  false
-else 
-  true 
-end
 end
